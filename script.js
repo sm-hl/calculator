3c5f896452f52750f56fb3c06b107ac1ce3a4b89
Salma Halhoul <halhoulsa00@gmail.com>
2024-01-21 23:17:10 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+//class donnee aux 4 operateurs + - * /
+let operator = document.querySelectorAll('.operator');
+
+let Chiffre = [];
+let Chiffres = [];
+let Operator = [];
+
+operator[0].addEventListener('click',function() {
+    
+})
